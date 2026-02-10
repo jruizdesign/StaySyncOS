e71474dcc69cf902566804867b41b023aad3697f
jruizdesign <jruizdesign@gmail.com>
2026-02-09 16:52:21 -0800

    feat: add a development bypass for admin login credentials.

--- src/environment.ts
+++ src/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
     supabaseUrl: 'https://hpgzwebgfjdwpaqwwoub.supabase.co',
 
     supabasePublishableKey: 'sb_publishable_V6omFqvFNDTuOwhGbH7sVA_kSRix8qx', // REPLACE with key starting with sb_publishable_ ("Publishable key")
-    geminiApiKey: 'AIzaSyDEuQF_LUE306zikbsASv5JV4SesKLc8_A'
+    geminiApiKey: '' // TODO: Set via environment variable or build script
 
 };
